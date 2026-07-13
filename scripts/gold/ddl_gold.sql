84c6461244ec505d1266dc5d4d17612845dbf762
Edgar Martinez <137846973+emartine0@users.noreply.github.com>
2026-07-13 16:24:11 -0600

    Create ddl_gold.sql

---
+++ scripts/gold/ddl_gold.sql
@@ -0,0 +1,56 @@
+/*
+===============================================================================
+Codigo DDL: Crear las Vistas 'gold'
+===============================================================================
+Propoósito del Codigo:
+    Este codigo crea las vistas para la capa 'gold'.
+    La capa 'gold' representa la tabla final.
+
+    Cada vista realiza transformaciones desde la capa 'silver' para 
+    producir un conjunto de datos limpio, enriquecido y listo para su uso 
+    en análisis.
+
+Uso:
+    - Estas vistas se pueden consultar directamente para análisis e informes.
+===============================================================================
+*/
+
+-- =============================================================================
+-- Creación: gold.quejas_telecom
+-- =============================================================================
+IF OBJECT_ID('gold.quejas_telecom', 'V') IS NOT NULL
+    DROP VIEW gold.quejas_telecom;
+GO
+
+CREATE VIEW gold.quejas_telecom AS
+SELECT
+	expediente, -- Reporte
+	medio_ingreso,
+	estado_procesal,
+	tipo_reclamacion,
+	motivo_reclamacion,
+	fecha_ingreso,
+	CASE expediente
+		WHEN 'PFC.HGO.B.3/001586-2022' THEN '2023-05-04'
+		WHEN 'PFC.YUC.B.3/001891-2022' THEN '2023-05-04'
+		WHEN 'PFC.ZAC.B.3/000867-2022' THEN '2023-05-02'
+		ELSE fecha_cierre
+	END AS fecha_cierre,
+	tipo_conciliacion,
+	CASE WHEN procedimiento = 'Conciliación por Medios Electrónicos' THEN 'Conciliación Medios Electrónicos'
+		 ELSE procedimiento
+	END AS procedimiento,
+	monto_reclamado,
+	monto_recuperado,
+	proveedor, -- proveedor
+	nombre_comercial,
+	giro,
+	tipo_producto, -- Producto
+	clase,
+	modalidad_compra,
+	modalidad_pago,
+	costo_bien_servicio,
+	odeco AS oficina_defensa_consumidor, -- Oficinas
+	estado
+FROM silver.quejas_telecom;
+GO
